fc0f186d1ecd8afffc4e182ab8d77fe89211855f
rickschubert <rick.schubert@youview.com>
2021-10-14 15:32:18 +0100

    two clients working

---
+++ clients/node/index.js
@@ -0,0 +1,24 @@
+const grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    __dirname + '../../../gamingstats/gaming_stats.proto',
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+const hello_proto = grpc.loadPackageDefinition(packageDefinition).gamingstats;
+
+function main() {
+    var client = new hello_proto.GamingStats('localhost:8000',
+                                         grpc.credentials.createInsecure());
+    client.getGame({title: 'Uncharted 4'}, function(err, response) {
+      if (err) {
+          throw err
+      }
+      console.log('Game found:', response);
+    });
+}
+
+main()
